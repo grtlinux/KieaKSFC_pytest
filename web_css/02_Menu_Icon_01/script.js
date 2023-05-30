168a36a060ea781c13209854d61b00e2c2e64671
Kiea Kang <grtlinux@gmail.com>
2023-05-30 13:14:31 +0900

    #add some samples

--- web_css/02_Menu_Icon_01/script.js
+++ web_css/02_Menu_Icon_01/script.js
@@ -1,10 +1,4 @@
-function init() {
-    document.getElementById('menu-icon').addEventListener('click', (x) => {
-        x.classList.toggle('change');
-    });
-}
-
 function myFunction(x) {
     x.classList.toggle("change");
-  }
+}
   
