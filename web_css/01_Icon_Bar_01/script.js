695a451e5aee19b3a8e27ed5e6f7170722bf40c8
Kiea Kang <grtlinux@gmail.com>
2023-05-30 09:05:06 +0900

    ...

---
+++ web_css/01_Icon_Bar_01/script.js
@@ -0,0 +1,9 @@
+// initialize
+function init() {
+    // alert('hello world')
+    
+    // document.getElementById('home').addEventListener('click', () => {
+    document.querySelector('#home').addEventListener('click', () => {
+        console.log('home')
+    });
+}
